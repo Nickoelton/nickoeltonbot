faab8187efec0e129630cd1d32b10d460a2b185a
Elton Rodriguez Dulanto <nick_22_04@hotmail.com>
2017-11-18 16:27:43 -0500

    Test 1
    QnA Maker test 1

---
+++ Models/AnswerDialog.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+namespace SimpleEchoBot.Models
+{
+    [Serializable]
+    [QnAMaker("f2229f47799a425ca7e36fc67e2144ba", "5178a5c5-4c15-444f-a9fd-19b43cd4c479")]
+    public class AnswerDialog : QnAMakerDialog
+    {
+       
+    }
+}   
